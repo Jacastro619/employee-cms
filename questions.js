5ceca94f286e8cde4a7c7e60c943a611299159bd
Jacastro619 <jorgecastro619@gmail.com>
2023-10-28 19:43:31 -0700

    added questions.js files to have questions for inquirer

---
+++ questions.js
@@ -0,0 +1,16 @@
+const initQuestions = [
+  {
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View All Departments",
+      "View All Roles",
+      "View All Employees",
+      "Add a Department",
+      "Add a Role",
+      "Add an Employee",
+      "Update an Employee Role",
+    ],
+    name: "initAction",
+  },
+];
